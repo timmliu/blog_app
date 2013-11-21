2180308d38dd9d8961bde7a960eb9f7a639ba1e9
timm.liu <timm.liu@activenetwork.com>
2013-11-21 13:18:23 -0800

    add password and password_confirmation tests and 'has_secure_password' to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :username, presence: true, length: { maximum: 25 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(username: "user1", email: "user1@example.com") }
+  before { @user = User.new(username: "user1", email: "user1@example.com", password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
@@ -46,7 +46,6 @@ describe User do
   		user_with_same_username = @user.dup
   		user_with_same_username.save
   	end
-
   	it { should_not be_valid }
   end
 
@@ -81,7 +80,18 @@ describe User do
       user_with_same_email = @user.dup
       user_with_same_email.save
     end
+    it { should_not be_valid }
+  end
 
+  describe "when password is not present" do
+    before do
+      @user = User.new(username: "user1", email: "user1@example.com", password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid}
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 end
