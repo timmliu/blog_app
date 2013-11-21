f9bc9c34ebe829d7f35c46d0f6d3787fd057d587
timm.liu <timm.liu@activenetwork.com>
2013-11-21 12:15:42 -0800

    add index to users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	before_save { self.username = username.downcase }
+	before_save { self.email = email.downcase }
 	VALID_USERNAME_REGEX = /\A[a-z]_?(?:[a-z0-9]_?)*\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :username, presence: true, length: { maximum: 25 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
