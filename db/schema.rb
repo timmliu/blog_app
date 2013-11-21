6a7d0f95cfe39edf926c10eea799542eea247af2
timm.liu <timm.liu@activenetwork.com>
2013-11-21 11:44:16 -0800

    create migration to rename :name column to :username

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121191951) do
+ActiveRecord::Schema.define(version: 20131121194228) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(username: "Example User", email: "user@example.com") }
 
   subject { @user }
 
-  it { should respond_to(:name) }
+  it { should respond_to(:username) }
   it { should respond_to(:email) }
 
   it { should be_valid }
 
-  describe "when name is not present" do
-  	before { @user.name = " " }
+  describe "when username is not present" do
+  	before { @user.username = " " }
   	it { should_not be_valid }
   end
 
-	describe "when name is too long" do
-  	before { @user.name = "a" * 26 }
+	describe "when username is too long" do
+  	before { @user.username = "a" * 26 }
   	it { should_not be_valid }
   end
 
