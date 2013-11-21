1b22b1843f5e8c6ba6c0475605eb3dcb985b853c
timm.liu <timm.liu@activenetwork.com>
2013-11-21 12:12:47 -0800

    fix user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	VALID_USERNAME_REGEX = /\A[a-z]_?(?:[a-z0-9]_?)*\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :username, presence: true, length: { maximum: 25 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: true
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+	validates :username, presence: true, length: { maximum: 25 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
