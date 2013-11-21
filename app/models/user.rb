756f8eb8014a045f738852f974bc49647574750e
timm.liu <timm.liu@activenetwork.com>
2013-11-21 13:49:13 -0800

    test and validate password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 	validates :username, presence: true, length: { maximum: 25 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
