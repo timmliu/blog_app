599cd7c57ddf869f118e2cca0a84e43776e0b8eb
timm.liu <timm.liu@activenetwork.com>
2013-12-28 09:23:14 -0800

    add definitions to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
