00d46074a7cd8cf5327fbc99182245578948271e
timm.liu <timm.liu@activenetwork.com>
2014-01-02 09:58:38 -0800

    add Factory Girl gem, sidebar css, show.html.erb, update routes, etc

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,14 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.username) }
+    it { should have_title(user.username) }
+  end
+
   describe "signup page" do
   	before { visit signup_path }
 
